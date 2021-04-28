8c0d713df83301835b25562efb6b57c14e0531d0
JoseLandivar <joseantoniolandivar@gmail.com>
2021-04-27 22:11:52 -0400

    dia-3

---
+++ SentenciasDeControl.py
@@ -0,0 +1,11 @@
+###   If/Else   ###
+
+condicion = 'Hola'
+
+if condicion == True:
+    print('Condicion Verdadera')
+elif condicion == False:
+    print('Condicion Falsa')
+else:
+    print('Condicion no reconocida')
+    
