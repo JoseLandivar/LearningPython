24581861604bead00e4fe26edbefd3c59c2d4519
JoseLandivar <joseantoniolandivar@gmail.com>
2021-04-28 23:29:53 -0400

    Dia4

--- SentenciasDeControl.py
+++ SentenciasDeControl.py
@@ -9,3 +9,27 @@ elif condicion == False:
 else:
     print('Condicion no reconocida')
     
+###    Conversion de numero a texto   ###
+
+numero = int(input("Ingrese un numero del 1 al 3: "))
+numeroTexto = ''
+
+    if numero == 1:
+        numeroTexto = 'Numero Uno'
+    elif numero == 2:
+        numeroTexto = 'Numero Dos'
+    elif numero == 3:
+        numeroTexto = 'Numero Tres'
+    else:
+        numeroTexto = 'Numero Fuera de Rango'
+    
+print(f'El numero es: {numeroTexto}')
+
+###    Version simplificada de If/Else    ###
+
+print('Condicion verdadera') if condicion else print('Condicion falsa')
+    
+    
+    
+    
+    
