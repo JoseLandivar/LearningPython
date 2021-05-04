d77a01606c92c2c4d9ce7731cb55f1cbf24f47d4
JoseLandivar <joseantoniolandivar@gmail.com>
2021-05-03 22:14:49 -0400

    Dia-8

---
+++ Practicas/Factorial.py
@@ -0,0 +1,10 @@
+num = int(input("Ingrese Factorial: "))
+i = 0
+if num > 0:
+   for i in num:
+        #    num = num - 1
+        #    num = num * num
+        print(i)
+else:
+    print('Numero Invalido')
+    
