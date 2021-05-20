059c5cf5fdabfd19b23aeaa6af4a615b523a111e
Landivar <joseantoniolandivar@gmail.com>
2021-05-20 08:05:01 -0400

    Dia-12

--- Practicas/EjercicioMasterenPython.py
+++ Practicas/EjercicioMasterenPython.py
@@ -55,3 +55,12 @@ print(caracter('E'))
 # 5- Escribir una función sum() y una función multip() que sumen y multipliquen
 # respectivamente todos los números de una lista. Por ejemplo: sum([1,2,3,4])
 # debería devolver 10 y multip([1,2,3,4]) debería devolver 24.
+
+def sum (lista):
+    suma = 0
+    for i in lista:
+        suma += i
+    return suma
+
+
+print (sum([1,2,3,4])) 
